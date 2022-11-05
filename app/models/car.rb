166af0a3fff3e5708ab941573ff17e99f340b453
TemporalSPACE <reverteadriel@gmail.com>
2022-11-05 11:55:27 -0300

    No recuerdo que cambie

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,8 @@ class Car < ApplicationRecord
 
 	has_many :rents
 	has_one_attached :photo
+
+	validates :patente, presence: true, uniqueness: true
+	validates :modelo, presence: true
+	validates_format_of :patente,:with => /\A([a-zA-Z]{2,3}[0-9]{3}[a-zA-Z]{0,2})?\z/
 end
