c2eae68361328b46d7fb641d36754cd761ef3bee
leesan2000 <lisandrovicens1@gmail.com>
2022-11-06 18:33:12 -0300

    validacion de usuarios ready

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_30_120212) do
 
   create_table "rents", force: :cascade do |t|
     t.float "precio", default: 0.0
-    t.datetime "fecha", default: "2022-11-05 23:15:06"
     t.float "combustible_gastado", default: 0.0
     t.datetime "tiempo"
     t.integer "car_id", null: false
