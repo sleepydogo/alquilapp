c5770f54ce8b014ec403b911392601c332590a94
tom <tomaschattmann@gmail.com>
2022-11-08 20:29:23 -0300

    todo mas pulido todavia B)

--- app/controllers/billetera_controller.rb
+++ app/controllers/billetera_controller.rb
@@ -3,13 +3,28 @@ class BilleteraController < ApplicationController
   def vista  
   end
 
-  def create_pago
-    @pago = Pago.new()
+  def pago
+    @pago = Pago.new(pago_params)
+
+    respond_to do |format|
+      if @pago.save
+        format.html { redirect_to pago_url(@pago), notice: "Pago creado, esperando confirmacion..." }
+        format.json { render :show, status: :created, location: @pago }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @pago.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
-  # GET /billetera/pago
-  def pago 
-    render 'billetera/_saldo-editer'
+  # Post /billetera/pago
+  def update_pago 
+    @pago = Pago.find(params[:id])
+    if @pago.update_attributes(pago_params)
+      redirect_to pagos_url
+    else  
+      render action: 'pagar'
+    end
   end
 
   def alquilapp_webhook
