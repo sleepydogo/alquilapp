79c843b23662c251891b78db9bb0fe24f61a0935
TemporalSPACE <reverteadriel@gmail.com>
2022-11-08 13:14:53 -0300

    Modelo pago 2

--- db/migrate/20221108161246_create_pagos.rb
+++ db/migrate/20221108161246_create_pagos.rb
@@ -1,12 +1,11 @@
 class CreatePagos < ActiveRecord::Migration[7.0]
   def change
     create_table :pagos do |t|
-      t.integer :usuario_id
       t.decimal :precio
       t.boolean :aceptado
       t.text :request
       t.text :response
-      t.datetime :fecha
+	  t.references :user, null: false, foreign_key: true	
 
       t.timestamps
     end
