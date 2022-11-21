2aa8851cdfb6941951be811fddd79c40414e487a
leesan2000 <lisandrovicens1@gmail.com>
2022-11-21 18:37:27 -0300

    redireccion a inicio en login

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -52,7 +52,7 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
-     cars_path
+     root_path
    end
 
   # The path used after sign up for inactive accounts.
