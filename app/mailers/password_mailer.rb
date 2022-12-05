800243fd276db6d055a4c003e8c4ebef9efc2760
leesan2000 <lisandrovicens1@gmail.com>
2022-12-05 19:56:41 -0300

    token password

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -3,6 +3,18 @@ class PasswordMailer < ApplicationMailer
     def reset_password
         @user = params[:user]
         @url = 'https://38fc-181-169-178-229.sa.ngrok.io'
+        create_reset_password_token(@user)
         mail(to: @user.email, subject: 'Cambio de contraseña')
     end
+    
+    private
+    def create_reset_password_token(user)
+        raw, hashed = Devise.token_generator.generate(User, :reset_password_token)}
+        @token = raw
+        user.reset_password_token = hashed
+        user.reset_password_sent_at = Time.now
+        user.save
+        puts Rails.application.routes.url_helpers.edit_user_password_url(reset_password_token: raw, host: 'localhost:3000')
+    end
+
 end
